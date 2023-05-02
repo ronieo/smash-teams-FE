3efee205b049e2ec43e365e803458ec806eb1248
kjun <kjun_all@naver.com>
2023-05-02 14:51:52 +0900

    feat: 공통 컴포넌트 추가
    - layout, navbar, sidebar

---
+++ src/components/common/profilebar/style.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+
+export const ProfileWrapper = styled.div`
+  width: 300px;
+  height: calc(100vh - 420px);
+  color: #fff;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #952a2a;
+`
+
+export const TeamTitle = styled.div`
+  font-size: 26px;
+  font-weight: 700;
+`
+export const ImageWrapper = styled.div`
+  width: 100px;
+  height: 100px;
+
+  min-height: 140px;
+  min-width: 140px;
+  border-radius: 50%;
+  background-color: #fff;
+  margin-top: 24px;
+`
+export const NameWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 26px;
+  line-height: 40px;
+`
+export const Position = styled.div`
+  font-size: 22px;
+  font-weight: 600;
+`
+export const Name = styled.div`
+  margin-left: 10px;
+  font-size: 20px;
+  font-weight: 600;
+`
+export const EmailWrapper = styled.div`
+  margin-top: auto;
+  font-size: 20px;
+  line-height: 30px;
+`
+export const PhoneWrapper = styled.div`
+  margin-top: 10px;
+  font-size: 20px;
+`
+export const StartWork = styled.div`
+  margin-top: 10px;
+  margin-top: auto;
+  line-height: 30px;
+`
+export const EditButton = styled.button`
+  color: #952a2a;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 10px 24px;
+  margin-top: 10px;
+  cursor: pointer;
+`
+export const EditTitle = styled.div`
+  font-size: 18px;
+  font-weight: 800;
+`
