35cde3767d9a714a761e51150faa0c6ad9858d73
ronieo <romelzen50@gmail.com>
2023-05-12 00:55:41 +0900

    fix: 연차페이지(dayOffPage), useLocation활용
    - style.ts: 랜더링될 화면 스타일링
    - index.tsx: 당직 페이지에 재사용 할 수 있도록 useLocation활용

--- src/pages/dayoffPage/index.tsx
+++ src/pages/dayoffPage/index.tsx
@@ -1,8 +1,18 @@
+import { useLocation } from 'react-router-dom'
+import MiniCalendar from '../../components/miniCalendar'
+import ShiftFormContainer from '../../components/shiftFormContainer'
 import { DayOffWrapper } from './style'
 
 // 연차/반차 신청 페이지
 function DayOffPage() {
-  return <DayOffWrapper>DayOffPage</DayOffWrapper>
+  const location = useLocation()
+  console.log('location 출력', location.pathname)
+  return (
+    <DayOffWrapper>
+      <MiniCalendar view={'month'} />
+      <ShiftFormContainer type={'dayOff'} location={location.pathname} children={undefined} />
+    </DayOffWrapper>
+  )
 }
 
 export default DayOffPage
