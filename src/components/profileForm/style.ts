4f5cbc55764ae4591a4de57d2e137d52bd51f5f8
Franz Cho <smscm@kakao.com>
2023-05-13 01:37:16 +0900

    feat: 개인정보수정 페이지 구현 - 개인정보수정 페이지 UI 컴포넌트 구현 - 개인정보수정 페이지 스타일 구현

---
+++ src/components/profileForm/style.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const ProfileWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh  - 400px);
+  margin: auto 0;
+  width: 600px;
+  height: 800px;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px 3px rgba(0, 0, 0, 0.25);
+  border-radius: 6px;
+`
+
+export const ProfileLogoImage = styled.img`
+
+`
+
+export const ProfileFormContainer = styled.form`
+
+`
+
+export const ProfileInput = styled.input`
+
+`
+
+export const ProfileModifyButton = styled.button`
+
+`
+
