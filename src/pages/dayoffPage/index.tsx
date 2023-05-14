ccb271fb3d6c16fb978f588becdcbf84567e4d63
ronieo <romelzen50@gmail.com>
2023-05-15 00:36:34 +0900

    feat: MiniCalendar, ShiftFormContainer 상태 추가 및 공동 props추가

--- src/pages/dayoffPage/index.tsx
+++ src/pages/dayoffPage/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
+
 import MiniCalendar from '../../components/miniCalendar'
 import ShiftFormContainer from '../../components/shiftFormContainer'
 import { DayOffWrapper } from './style'
@@ -7,10 +9,24 @@ import { DayOffWrapper } from './style'
 function DayOffPage() {
   const location = useLocation()
   console.log('location 출력', location.pathname)
+
+  const [startDate, setStartDate] = useState<string>('')
+  const [endDate, setEndDate] = useState<string>('')
+  const [reason, setReason] = useState<string>('')
+
   return (
     <DayOffWrapper>
-      <MiniCalendar view={'month'} />
-      <ShiftFormContainer type={'dayOff'} location={location.pathname} children={undefined} />
+      <MiniCalendar view={'month'} setStartDate={setStartDate} setEndDate={setEndDate} setReason={setReason} />
+
+      <ShiftFormContainer
+        type={'DAYOFF'}
+        location={location.pathname}
+        children={undefined}
+        startDate={startDate}
+        endDate={endDate}
+        reason={reason}
+        // dateFormat={''}
+      />
     </DayOffWrapper>
   )
 }
