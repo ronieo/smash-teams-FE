6943bda4794cac5eda1e34e4e7baba447a433518
ronieo <romelzen50@gmail.com>
2023-05-15 00:31:51 +0900

    fix: 연차, 반차, 당직 입력폼 로직 추가
    - FormType upper case로 수정
    - handleHalfOffButtonClick: 날짜 포맷, mutate함수 추가

--- src/components/shiftFormContainer/index.tsx
+++ src/components/shiftFormContainer/index.tsx
@@ -1,21 +1,25 @@
+import { AxiosError } from 'axios'
 import React, { useState, ReactNode, Dispatch, SetStateAction } from 'react'
+import { useMutation, useQueryClient } from 'react-query'
+import { createUserSchedule } from '../../apis/services/Schedule'
+import { ScheduleEnroll, ScheduleEnrollResponse } from '../../interface/schedule'
+
 import * as S from './style'
 import Swal from 'sweetalert2'
-import DropDown from '../common/dropdown'
 import { theme } from '../../styles/Theme'
-import { createUserSchedule } from '../../apis/services/Schedule'
+import { useLocation } from 'react-router-dom'
 
-type FormType = 'dayOff' | 'halfOff' | 'nightShift'
+type FormType = 'DAYOFF' | 'HALFOFF' | 'NIGHTSHIFT'
 const FORM_TYPES = {
-  DAY_OFF: 'dayOff',
-  HALF_OFF: 'halfOff',
-  NIGHT_SHIFT: 'nightShift',
+  DAY_OFF: 'DAYOFF',
+  HALF_OFF: 'HALFOFF',
+  NIGHT_SHIFT: 'NIGHTSHIFT',
 } as const
 
-interface ShiftFormContainerProps {
+export interface ShiftFormContainerProps {
   children: ReactNode
-  type: string
   location: string
+  type: string
   startDate: string
   endDate: string
   reason: string
@@ -23,26 +27,75 @@ interface ShiftFormContainerProps {
 
 function ShiftFormContainer({ type, location, startDate, endDate, reason }: ShiftFormContainerProps) {
   const [formType, setFormType] = useState<FormType>(FORM_TYPES.DAY_OFF)
-  const handleTitleChange = location === '/dayoff' ? (formType === FORM_TYPES.DAY_OFF ? '연차' : '반차') : '당직'
+  const handleTitleChange = location === '/dayoff' ? (formType === 'DAYOFF' ? '연차' : '반차') : '당직'
   const handleStartInputChange =
-    location === '/dayoff' ? (formType === 'dayOff' ? '시작 날짜' : '반차 시작') : '근무 시작'
+    location === '/dayoff' ? (formType === 'DAYOFF' ? '시작 날짜' : '반차 시작') : '근무 시작'
   const handleEndInputChange =
-    location === '/dayoff' ? (formType === 'dayOff' ? '종료 날짜' : '반차 종료') : '근무 종료'
+    location === '/dayoff' ? (formType === 'DAYOFF' ? '종료 날짜' : '반차 종료') : '근무 종료'
 
   const handleDayOffButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    setFormType(FORM_TYPES.DAY_OFF)
-    console.log(formType, '연차버튼 클릭!')
+    const typeDayOff = setFormType(FORM_TYPES.DAY_OFF)
+    console.log('연차버튼 클릭!', typeDayOff)
   }
 
   const handleHalfOffButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    setFormType(FORM_TYPES.HALF_OFF)
-    console.log('반차버튼 클릭!')
+    const typeHalfOff = setFormType(FORM_TYPES.HALF_OFF)
+    console.log('반차버튼 클릭!', typeHalfOff)
   }
 
-  const handleConfirmButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  function formatDate(dateString: string | number): string {
+    const date = new Date(dateString)
+    const year = date.getFullYear()
+    const month = ('0' + (date.getMonth() + 1)).slice(-2)
+    const day = ('0' + date.getDate()).slice(-2)
+    return `${year}.${month}.${day}`
+  }
+  const formatStartDate = formatDate(startDate) as string
+  const formatEndDate = formatDate(endDate) as string
+
+  const data = {
+    type: '',
+    startDate: '',
+    endDate: '',
+    reason: '',
+  }
+
+  const queryClient = useQueryClient()
+
+  const { mutate, isError, isLoading } = useMutation<ScheduleEnrollResponse, AxiosError, ScheduleEnroll>(
+    () =>
+      createUserSchedule({
+        schedule: { type, startDate: isSDate, endDate: isEDate, reason },
+        id: 5,
+      }),
+    {
+      onSuccess: (data) => {
+        console.log(data)
+      },
+      onError: (error: AxiosError) => {
+        console.log(error)
+      },
+    },
+  )
+  // const { d } = startDate
+  // console.log(d)
+  let isSDate
+  let isEDate
+
+  if (startDate && endDate) {
+    const SDate = new Date(startDate?.d)
+    const EDate = new Date(endDate?.d)
+    isSDate = SDate.toISOString().slice(0, 19)
+    isEDate = EDate.toISOString().slice(0, 19)
+    console.log('isSDate>>>>>', isSDate)
+    console.log('isEDate>>>>>', isEDate)
+  }
+
+  function handleConfirmButtonClick(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault()
+
     Swal.fire({
       title: `${handleTitleChange} 신청하시겠어요?`,
       text: '신청 후 취소하실 수 없습니다!',
@@ -60,6 +113,8 @@ function ShiftFormContainer({ type, location, startDate, endDate, reason }: Shif
           icon: 'success',
           confirmButtonColor: theme.colors.blue,
           confirmButtonText: '내역보기 페이지 바로가기',
+        }).then(() => {
+          mutate()
         })
       }
     })
@@ -83,29 +138,28 @@ function ShiftFormContainer({ type, location, startDate, endDate, reason }: Shif
             id="startDate"
             type="text"
             name="startDate"
-            value={startDate}
-            readOnly
+            value={formatStartDate}
           />
         </S.InputWrapper>
       ) : (
         <S.InputWrapper>
           <S.Label htmlFor="startDate halfDate">{handleStartInputChange}</S.Label>
-          <S.DateInput id="endDate" type="text" name="endDate" value="" />{' '}
+          <S.DateInput id="endDate" type="text" name="endDate" value={formatEndDate} />
         </S.InputWrapper>
       )}
       {location === '/dayoff' ? (
         <S.InputWrapper>
           <S.Label htmlFor="endDate halfOffTime">{handleEndInputChange}</S.Label>
-          {formType === 'dayOff' ? (
-            <S.DateInput id="endDate" type="text" name="endDate" value={endDate} readOnly />
+          {formType === 'DAYOFF' ? (
+            <S.DateInput id="endDate" type="text" name="endDate" value={formatEndDate} />
           ) : (
-            <S.TimeInput id="endDate" type="text" name="endDate" value={endDate} readOnly />
+            <S.TimeInput id="endDate" type="text" name="endDate" value={formatEndDate} />
           )}
         </S.InputWrapper>
       ) : (
         <S.InputWrapper>
           <S.Label htmlFor="startDate halfDate">{handleEndInputChange}</S.Label>
-          <S.DateInput id="endDate" type="text" name="endDate" value={endDate} readOnly />{' '}
+          <S.DateInput id="endDate" type="text" name="endDate" value={formatEndDate} />
         </S.InputWrapper>
       )}
       <S.InputWrapper>
