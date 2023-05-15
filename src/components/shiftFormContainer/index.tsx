e1d64974ce1362bd99a1477538bb7ab47516077a
ronieo <romelzen50@gmail.com>
2023-05-15 16:40:22 +0900

    fix: 승인요청 후 내역보기 페이지 리다이렉션
    - 최종 승인 단계를 마치면 내역보기 페이지로 넘어가게 하기위해
      useEffect로 분기

--- src/components/shiftFormContainer/index.tsx
+++ src/components/shiftFormContainer/index.tsx
@@ -1,5 +1,5 @@
 import { AxiosError } from 'axios'
-import React, { useState, ReactNode, Dispatch, SetStateAction } from 'react'
+import React, { useState, ReactNode, Dispatch, SetStateAction, useEffect } from 'react'
 import { useMutation, useQueryClient } from 'react-query'
 import { createUserSchedule } from '../../apis/services/Schedule'
 import { ScheduleEnroll, ScheduleEnrollResponse } from '../../interface/schedule'
@@ -7,7 +7,7 @@ import { ScheduleEnroll, ScheduleEnrollResponse } from '../../interface/schedule
 import * as S from './style'
 import Swal from 'sweetalert2'
 import { theme } from '../../styles/Theme'
-import { useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 type FormType = 'DAYOFF' | 'HALFOFF' | 'NIGHTSHIFT'
 const FORM_TYPES = {
@@ -26,6 +26,8 @@ export interface ShiftFormContainerProps {
 }
 
 function ShiftFormContainer({ type, location, startDate, endDate, reason }: ShiftFormContainerProps) {
+  const navigate = useNavigate()
+  const [confirmed, setConfirmed] = useState(false)
   const [formType, setFormType] = useState<FormType>(FORM_TYPES.DAY_OFF)
   const handleTitleChange = location === '/dayoff' ? (formType === 'DAYOFF' ? '연차' : '반차') : '당직'
   const handleStartInputChange =
@@ -62,13 +64,11 @@ function ShiftFormContainer({ type, location, startDate, endDate, reason }: Shif
     reason: '',
   }
 
-  const queryClient = useQueryClient()
-
   const { mutate, isError, isLoading } = useMutation<ScheduleEnrollResponse, AxiosError, ScheduleEnroll>(
     () =>
       createUserSchedule({
         schedule: { type, startDate: isSDate, endDate: isEDate, reason },
-        id: 5,
+        id: id,
       }),
     {
       onSuccess: (data) => {
@@ -79,8 +79,7 @@ function ShiftFormContainer({ type, location, startDate, endDate, reason }: Shif
       },
     },
   )
-  // const { d } = startDate
-  // console.log(d)
+
   let isSDate
   let isEDate
 
@@ -95,7 +94,6 @@ function ShiftFormContainer({ type, location, startDate, endDate, reason }: Shif
 
   function handleConfirmButtonClick(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault()
-
     Swal.fire({
       title: `${handleTitleChange} 신청하시겠어요?`,
       text: '신청 후 취소하실 수 없습니다!',
@@ -107,81 +105,91 @@ function ShiftFormContainer({ type, location, startDate, endDate, reason }: Shif
       cancelButtonText: '취소할게요!',
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: '신청!',
-          text: `신청이 완료되었어요! 내역보기에서 확인해보세요 :)`,
-          icon: 'success',
-          confirmButtonColor: theme.colors.blue,
-          confirmButtonText: '내역보기 페이지 바로가기',
-        }).then(() => {
-          mutate()
-        })
+        setConfirmed(true)
       }
     })
   }
 
+  useEffect(() => {
+    if (confirmed) {
+      Swal.fire({
+        title: '신청!',
+        text: '신청이 완료되었어요! 내역보기에서 확인해보세요 :)',
+        icon: 'success',
+        confirmButtonColor: theme.colors.blue,
+        confirmButtonText: '내역보기 페이지 바로가기',
+      }).then(() => {
+        mutate()
+        navigate('/history')
+      })
+    }
+  })
+
   return (
-    <S.RedContainerWrapper>
-      {location === '/dayoff' ? (
-        <S.DayOffCountContainer>
-          <p>남은 연차 날짜</p>
-          {/* <p>`Day-${'dayOffCount'}`</p> */}
-          <p>Day-17.5</p>
-        </S.DayOffCountContainer>
-      ) : null}
-      <S.ShiftTitle>{handleTitleChange}</S.ShiftTitle>
-      {location === '/dayoff' ? (
+    <>
+      <S.RedContainerWrapper>
+        {location === '/dayoff' ? (
+          <S.DayOffCountContainer>
+            <p>남은 연차 날짜</p>
+            {/* <p>`Day-${'dayOffCount'}`</p> */}
+            <p>Day-17.5</p>
+          </S.DayOffCountContainer>
+        ) : null}
+        <S.ShiftTitle>{handleTitleChange}</S.ShiftTitle>
+        {location === '/dayoff' ? (
+          <S.InputWrapper>
+            <S.Label htmlFor="startDate halfDate">{handleStartInputChange}</S.Label>
+            <S.DateInput
+              className="startDate halfDate"
+              id="startDate"
+              type="text"
+              name="startDate"
+              value={formatStartDate}
+              readOnly
+            />
+          </S.InputWrapper>
+        ) : (
+          <S.InputWrapper>
+            <S.Label htmlFor="startDate halfDate">{handleStartInputChange}</S.Label>
+            <S.DateInput id="endDate" type="text" name="endDate" value={formatEndDate} readOnly />
+          </S.InputWrapper>
+        )}
+        {location === '/dayoff' ? (
+          <S.InputWrapper>
+            <S.Label htmlFor="endDate halfOffTime">{handleEndInputChange}</S.Label>
+            {formType === 'DAYOFF' ? (
+              <S.DateInput id="endDate" type="text" name="endDate" value={formatEndDate} readOnly />
+            ) : (
+              <S.TimeInput id="endDate" type="text" name="endDate" value={formatEndDate} readOnly />
+            )}
+          </S.InputWrapper>
+        ) : (
+          <S.InputWrapper>
+            <S.Label htmlFor="startDate halfDate">{handleEndInputChange}</S.Label>
+            <S.DateInput id="endDate" type="text" name="endDate" value={formatEndDate} readOnly />
+          </S.InputWrapper>
+        )}
         <S.InputWrapper>
-          <S.Label htmlFor="startDate halfDate">{handleStartInputChange}</S.Label>
-          <S.DateInput
-            className="startDate halfDate"
-            id="startDate"
-            type="text"
-            name="startDate"
-            value={formatStartDate}
-          />
+          <S.Label htmlFor="reason">사유</S.Label>
+          <S.Input id="reason" type="text" name="reason" value={reason} readOnly />
         </S.InputWrapper>
-      ) : (
-        <S.InputWrapper>
-          <S.Label htmlFor="startDate halfDate">{handleStartInputChange}</S.Label>
-          <S.DateInput id="endDate" type="text" name="endDate" value={formatEndDate} />
-        </S.InputWrapper>
-      )}
-      {location === '/dayoff' ? (
-        <S.InputWrapper>
-          <S.Label htmlFor="endDate halfOffTime">{handleEndInputChange}</S.Label>
-          {formType === 'DAYOFF' ? (
-            <S.DateInput id="endDate" type="text" name="endDate" value={formatEndDate} />
-          ) : (
-            <S.TimeInput id="endDate" type="text" name="endDate" value={formatEndDate} />
-          )}
-        </S.InputWrapper>
-      ) : (
-        <S.InputWrapper>
-          <S.Label htmlFor="startDate halfDate">{handleEndInputChange}</S.Label>
-          <S.DateInput id="endDate" type="text" name="endDate" value={formatEndDate} />
-        </S.InputWrapper>
-      )}
-      <S.InputWrapper>
-        <S.Label htmlFor="reason">사유</S.Label>
-        <S.Input id="reason" type="text" name="reason" value={reason} />
-      </S.InputWrapper>
-      {location === '/dayoff' ? (
-        <S.ButtonsWrapper>
-          <S.DayOffButton id="dayOff" type="submit" onClick={handleDayOffButtonClick}>
-            연차
-          </S.DayOffButton>
-          <S.HalfOffButton id="halfOff" type="submit" onClick={handleHalfOffButtonClick}>
-            반차
-          </S.HalfOffButton>
-        </S.ButtonsWrapper>
-      ) : (
-        <S.SizedBox />
-      )}
-      <S.ApprovalRequestButton type="submit" onClick={handleConfirmButtonClick}>
-        승인요청하기
-      </S.ApprovalRequestButton>
-    </S.RedContainerWrapper>
+        {location === '/dayoff' ? (
+          <S.ButtonsWrapper>
+            <S.DayOffButton id="dayOff" type="submit" onClick={handleDayOffButtonClick}>
+              연차
+            </S.DayOffButton>
+            <S.HalfOffButton id="halfOff" type="submit" onClick={handleHalfOffButtonClick}>
+              반차
+            </S.HalfOffButton>
+          </S.ButtonsWrapper>
+        ) : (
+          <S.SizedBox />
+        )}
+        <S.ApprovalRequestButton type="submit" onClick={handleConfirmButtonClick}>
+          승인요청하기
+        </S.ApprovalRequestButton>
+      </S.RedContainerWrapper>
+    </>
   )
 }
 
