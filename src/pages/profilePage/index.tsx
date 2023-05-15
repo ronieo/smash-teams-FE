6d4e4bbe7c36709c75dce46d9ef50e839ec61b0b
kjun <kjun_all@naver.com>
2023-05-16 08:06:48 +0900

    feat: 회원가입 페이지 로직 수정 및 타입지정

--- src/pages/profilePage/index.tsx
+++ src/pages/profilePage/index.tsx
@@ -1,22 +1,9 @@
 import ProfileForm from '../../components/profileForm'
 
-
-// 프로필 페이지
-/**
- * TODO:
- * 1. 프로필 페이지 레이아웃 구성
- * 2. 내정보 조회 API 연동 / useQuery /get
- * 3. 정보수정 API 연동 / useMutation / post
- * 4. 조회한 정보를 input에 vaule로 넣어주기
- * 5. 수정하기 버튼 클릭시 정보 수정
- *
- */
-
 function ProfilePage() {
-
   return (
     <>
-      <ProfileForm/>
+      <ProfileForm />
     </>
   )
 }
