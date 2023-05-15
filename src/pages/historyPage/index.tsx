db61192d94d4634951dc7c2fe136bf7bee43980b
kjun <kjun_all@naver.com>
2023-05-15 17:07:28 +0900

    feat: 스케줄 내역, 관리 페이지컴포넌트/Swiper 컴포넌트 로직 수정

--- src/pages/historyPage/index.tsx
+++ src/pages/historyPage/index.tsx
@@ -1,17 +1,43 @@
 // 연차/당직 내역보기 페이지
 
-import { useQuery } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 import * as S from './style'
 
 import { getUserSchedule } from '../../apis/services/Schedule'
-import HalfOffShiftForm from '../../components/HalfOffShiftForm'
+import HalfOffShiftForm from '../../components/halfOffShiftForm'
+import { MyScheduleData } from '../../interface/schedule'
+import { AxiosError } from 'axios'
+import { LoginResponseData } from '../../apis/interface/Auth'
+import { getUser } from '../../apis/services/Auth'
 
 function HistoryPage() {
-  const { data: mySchedule, isError, isLoading } = useQuery('userHistory', getUserSchedule)
-  const scheduleData = mySchedule?.data.scheduleList
+  const { data: myUser } = useQuery<LoginResponseData, AxiosError>('myUser', getUser)
+
+  const userId = myUser?.data?.id
+
+  const {
+    data: mySchedule,
+    isLoading,
+    error,
+  } = useQuery<MyScheduleData[], AxiosError>('mySchedule', () => getUserSchedule(userId), {
+    enabled: !!userId, // userId가 존재할 때만 쿼리 실행
+  })
+
+  const scheduleData = mySchedule?.data?.scheduleList
+  console.log('첫 부분', scheduleData)
+
+  // return <S.HistoryContainer> {scheduleData && <HalfOffShiftForm scheduleData={scheduleData} />}</S.HistoryContainer>
   return (
     <S.HistoryContainer>
-      <HalfOffShiftForm scheduleData={scheduleData} />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error.message}</div>
+      ) : scheduleData ? (
+        <HalfOffShiftForm scheduleData={scheduleData} />
+      ) : (
+        <div>No schedule data.</div>
+      )}
     </S.HistoryContainer>
   )
 }
