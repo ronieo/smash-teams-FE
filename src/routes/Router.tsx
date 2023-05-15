506eafaf44a4b09b28d39ffee7c0d6d706cdda70
kjun <kjun_all@naver.com>
2023-05-15 17:10:50 +0900

    feat: ProtectedRouter적용

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -9,6 +9,7 @@ import NightSheetPage from '../pages/nightsheetPage'
 import ProfilePage from '../pages/profilePage'
 import HistoryPage from '../pages/historyPage'
 import SideBar from '../components/common/sidebar'
+import ProtectedRouter from './ProtectedRouter'
 
 function Router() {
   return (
@@ -16,19 +17,21 @@ function Router() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<SideBar />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/admin" element={<AdminPage />}>
-            <Route path=":id" element={<AdminPage />} />
+        <Route element={<ProtectedRouter />}>
+          <Route path="/" element={<SideBar />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/admin" element={<AdminPage />}>
+              <Route path=":id" element={<AdminPage />} />
+            </Route>
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/dayoff" element={<DayOffPage />} />
+            <Route path="/history" element={<HistoryPage />} />
+            <Route path="/manage" element={<ManagePage />} />
+            <Route path="/nightsheet" element={<NightSheetPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
           </Route>
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/dayoff" element={<DayOffPage />} />
-          <Route path="/history" element={<HistoryPage />} />
-          <Route path="/manage" element={<ManagePage />} />
-          <Route path="/nightsheet" element={<NightSheetPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
