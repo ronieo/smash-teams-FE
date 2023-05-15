ce3dbc60292d16f613c1cfc3e32b516508ee25b7
kjun <kjun_all@naver.com>
2023-05-16 08:04:07 +0900

    feat: 헤더, 사이드바 코드 수정

--- src/components/common/profilebar/style.ts
+++ src/components/common/profilebar/style.ts
@@ -1,14 +1,15 @@
 import styled from 'styled-components'
+import { theme } from '../../../styles/Theme'
 
 export const ProfileWrapper = styled.div`
   width: 280px;
   height: calc(100vh - 380px);
-  color: #fff;
+  color: ${theme.colors.white};
   padding: 40px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #952a2a;
+  background-color: ${theme.colors.red};
   min-height: 440px;
 `
 
@@ -18,7 +19,9 @@ export const TeamTitle = styled.div`
 `
 export const ImageWrapper = styled.div``
 
-export const UploadButton = styled.label``
+export const UploadButton = styled.label`
+  position: relative;
+`
 export const ProfileImageWrapper = styled.div`
   width: 100px;
   height: 100px;
@@ -32,11 +35,28 @@ export const ProfileImageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #fff;
+  background-color: ${theme.colors.white};
+  border: 2px solid ${theme.colors.red};
+  cursor: pointer;
+  &:hover {
+    border: 2px solid ${theme.colors.white};
+  }
 `
 export const ProfileImage = styled.img`
-  width: 100%;
+  height: 100%;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `
+
+export const AddIcon = styled.img`
+  width: 40px;
+  position: absolute;
+  top: 76px;
+  left: 76px;
+`
+
 export const ProfileInput = styled.input`
   display: none;
 `
@@ -57,25 +77,45 @@ export const Name = styled.div`
   font-weight: 600;
 `
 export const EmailWrapper = styled.div`
+  display: flex;
+  align-items: end;
   margin-top: 16px;
   font-size: 18px;
-  line-height: 30px;
+  svg {
+    margin-right: 10px;
+    font-size: 20px;
+  }
 `
 export const PhoneWrapper = styled.div`
+  display: flex;
+  align-items: end;
   margin-top: 6px;
   font-size: 16px;
+  svg {
+    margin-right: 10px;
+    font-size: 20px;
+  }
 `
 export const StartWork = styled.div`
   margin-top: 30px;
-  line-height: 20px;
+  font-weight: 500;
+  display: flex;
+  align-items: end;
+  svg {
+    margin-right: 10px;
+    font-size: 20px;
+  }
 `
 export const EditButton = styled.button`
-  color: #952a2a;
-  background-color: #fff;
+  color: ${theme.colors.red};
+  background-color: ${theme.colors.white};
   border-radius: 8px;
   padding: 10px 24px;
   cursor: pointer;
-  margin-top: auto;
+  margin-top: 20px;
+  &:hover {
+    background-color: ${theme.colors.gray};
+  }
 `
 export const EditTitle = styled.div`
   font-size: 18px;
