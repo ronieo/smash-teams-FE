6551e96c8dd6ca5a89597521679e907bb8f930f0
kjun <kjun_all@naver.com>
2023-05-16 14:06:02 +0900

    feat: 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://52.78.70.225:7777',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
